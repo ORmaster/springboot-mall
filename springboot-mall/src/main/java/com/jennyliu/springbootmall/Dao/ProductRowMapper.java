1ce674e441c7b1f85c60183c52db0341eababdcb
LP-0085\JennyLiu <smartjenny@pollex.com.tw>
2025-09-25 11:54:21 +0800

    商品功能 - 格式化返回的時間，並設定時區

--- springboot-mall/src/main/java/com/jennyliu/springbootmall/Dao/ProductRowMapper.java
+++ springboot-mall/src/main/java/com/jennyliu/springbootmall/Dao/ProductRowMapper.java
@@ -1,5 +1,6 @@
 package com.jennyliu.springbootmall.Dao;
 
+import com.jennyliu.springbootmall.Constant.ProductCategory;
 import com.jennyliu.springbootmall.model.Product;
 import org.springframework.jdbc.core.RowMapper;
 import org.springframework.stereotype.Component;
@@ -15,13 +16,13 @@ public class ProductRowMapper implements RowMapper<Product> {
 
         product.setProduct_id(rs.getInt("product_id"));
         product.setProduct_name(rs.getString("product_name"));
-        product.setCategory(rs.getString("category"));
+        product.setCategory(ProductCategory.valueOf( rs.getString("category")));
         product.setImage_url(rs.getString("image_url"));
         product.setPrice(rs.getInt("price"));
         product.setStock(rs.getInt("stock"));
         product.setDescription(rs.getString("description"));
-        product.setCreated_date(rs.getDate("created_date"));
-        product.setLast_modified_date(rs.getDate("last_modified_date"));
+        product.setCreated_date(rs.getTimestamp("created_date"));
+        product.setLast_modified_date(rs.getTimestamp("last_modified_date"));
 
         return product;
     }
